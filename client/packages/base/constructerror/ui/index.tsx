ab1e965000fa88a5e7bde1c2e594b678fcc7f381
Stefan Mogren <stefan_mogren@hotmail.com>
2025-11-19 15:53:31 +0100

    Finished adjusting design

--- client/packages/base/constructerror/ui/index.tsx
+++ client/packages/base/constructerror/ui/index.tsx
@@ -7,12 +7,14 @@ type DataContent = {
 };
 export const ConstructError = ({ color, title, text }: DataContent) => {
 	return (
-		<section className="info__container bg-super-light-beige">
+		<section className="info bg-super-light-beige">
 			<section className={`circle ${color}`}>
 				<img src="/assets/socket-icon.svg" className="info__img" alt="" />
 			</section>
-			<h2 className="heading-2">{title}</h2>
-			<p className="base info__text">{text}</p>
+			<section className="info__container">
+				<h2 className="heading-2">{title}</h2>
+				<p className="base info__text">{text}</p>
+			</section>
 			<a
 				href="/"
 				className="btn-base info__return-link bg-cucumber btn-text text-light-beige"
