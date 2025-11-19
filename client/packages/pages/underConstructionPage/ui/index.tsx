5e1e89b42c88a10c62a391267b629a2a987ea741
Stefan Mogren <stefan_mogren@hotmail.com>
2025-11-19 15:08:13 +0100

    Finished styling for UnderConstruction

--- client/packages/pages/underConstructionPage/ui/index.tsx
+++ client/packages/pages/underConstructionPage/ui/index.tsx
@@ -1,6 +1,22 @@
 import './index.scss';
-console.log('Construction page is loaded');
-
 export const UnderConstructionPage = () => {
-	return <div>This is my construction page</div>;
+	return (
+		<section className="info__container bg-light-beige">
+			<section className="circle bg-cucumber">
+				<img src="/assets/socket-icon.svg" className="info__img" alt="" />
+			</section>
+			<h2 className="heading-2">Under uppbyggnad</h2>
+			<p className="base info__text">
+				Just nu jobbar vi på något nytt och spännande här. Men oroa dig inte -
+				menyn och beställningen funkar precis som vanligt. Kom tillbaka snart
+				och upptäck vår nya moj!
+			</p>
+			<a
+				href="/"
+				className="btn-base info__return-link bg-cucumber btn-text text-light-beige"
+			>
+				Till startsidan
+			</a>
+		</section>
+	);
 };
