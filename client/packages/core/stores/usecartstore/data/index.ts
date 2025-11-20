88b5e9b6f330a9fac94d879ce0cd4453b1485371
lammai442 <lam.mai442@gmail.com>
2025-11-20 14:32:27 +0100

    Working on useCartStore

--- client/packages/core/stores/usecartstore/data/index.ts
+++ client/packages/core/stores/usecartstore/data/index.ts
@@ -9,13 +9,13 @@ import type { Meal } from '@mojjen/productdata';
 type CartItem = {
 	product: Meal;
 	qty: number;
-}
+};
 
 type CartStore = {
 	cartCount: number;
 	cart: CartItem[];
 	increase: (product: Meal) => void;
-	decrease: (id: string) => void;
+	// decrease: (id: string) => void;
 };
 
 export const useCartStore = create<CartStore>((set) => ({
@@ -23,19 +23,25 @@ export const useCartStore = create<CartStore>((set) => ({
 	cart: [],
 	increase: (product) =>
 		set((state) => {
-			const existing = state.cart.find((item) => item.product.id === product.id);
-			if(existing) {
-				 return {
-        cart: state.cart.map((item) =>
-          item.product.id === product.id
-            ? { ...item, qty: item.qty + 1 }
-            : item
-        ),
-		cartCount: state.cartCount + 1,
-      }}),
-	decrease: (productId) =>
-		set((state) => ({
-			cartCount: state.cartCount - 1,
-			cart: ,
-		})),
+			const existingInCart = state.cart.find(
+				(item) => item.product.id === product.id
+			);
+
+			if (existingInCart) {
+				return {
+					cart: state.cart.map((item) =>
+						item.product.id === product.id
+							? { ...item, qty: item.qty + 1 }
+							: item
+					),
+					cartCount: state.cartCount + 1,
+				};
+			}
+
+			// If product doesnt exist
+			return {
+				cart: [...state.cart, { product, qty: 1 }],
+				cartCount: state.cartCount + 1,
+			};
+		}),
 }));
