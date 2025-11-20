501ba68252060c977546de4bae25426fdd169039
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-11-20 14:23:41 +0100

    Cart page that renders product cards containing test data added.

--- client/packages/base/footer/ui/index.tsx
+++ client/packages/base/footer/ui/index.tsx
@@ -7,7 +7,7 @@ import './index.scss';
 
 export const FooterComp = () => {
 	return (
-		<footer className="footer ">
+		<footer className="footer bg-light-beige">
 			{/**
 			 * * ----- Kontakt -----
 			 */}
