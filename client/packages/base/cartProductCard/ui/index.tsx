155ecbdedac4f16d3f612cd1c95fdaea3c8d130d
Stefan Mogren <stefan_mogren@hotmail.com>
2025-11-27 13:46:04 +0100

    Fixed two classnames in cartProductCard

--- client/packages/base/cartProductCard/ui/index.tsx
+++ client/packages/base/cartProductCard/ui/index.tsx
@@ -11,6 +11,9 @@ import { IoClose } from 'react-icons/io5';
 /**
  * Author: Klara
  * Product card displayed in cart view.
+ *
+ * Bugfix: StefanMogren
+ * Fixed classname of product-card__img and product-card__img-box
  */
 
 type Props = {
@@ -39,7 +42,7 @@ export const CartProductCard = ({
 		if (!itemExistInCart) setQuantity(0);
 	}, [cart]);
 
-	const imgClassNames = clsx('product-card__img2', {
+	const imgClassNames = clsx('product-card__img', {
 		inactive: status === 'inactive',
 	});
 
@@ -48,7 +51,7 @@ export const CartProductCard = ({
 			<ContentBox extraClass="flex__row cart-item">
 				<li className="flex flex__gap-1  cart-item__list-item ">
 					<div
-						className={`product-card__img-box2 flex flex__column ${classBgColor}`}
+						className={`product-card__img-box flex flex__column ${classBgColor}`}
 					>
 						<img className={imgClassNames} src={img} alt="Image of meal" />
 					</div>
