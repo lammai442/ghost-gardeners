7ef8d973e5ab3cc8070392d386e97558f25ab49f
Stefan Mogren <stefan_mogren@hotmail.com>
2025-12-01 15:35:45 +0100

    Properly named order folder, adjusted SCSS

--- business/packages/pages/orderpage/ui/index.tsx
+++ business/packages/pages/orderpage/ui/index.tsx
@@ -1,15 +1,21 @@
 import './index.scss';
 import { Order } from '@mojjen/order';
 
+/**
+ * Author: StefanMogren
+ * Created OrderPage.
+ *
+ */
+
 export const OrderPage = () => {
 	return (
 		<main className="order-page">
-			<section className="order-page__container">
-				<h1>Ordrar</h1>
+			<section className="order-page__container flex flex__column flex__gap-3">
+				<h1 className="heading-1">Ordrar</h1>
 
 				<section className="order-page__orders">
-					<section>
-						<h2 className="heading-3">Väntande (10 st)</h2>
+					<section className="flex flex__column flex__gap-2">
+						<h2 className="heading-3">Väntande (5 st)</h2>
 						<ul className="order-page__orders-container">
 							<Order orderId="di7bv" time="14.33" orderStatus="pending" />
 							<Order orderId="di7bv" time="14.33" orderStatus="pending" />
@@ -18,7 +24,7 @@ export const OrderPage = () => {
 							<Order orderId="di7bv" time="14.33" orderStatus="pending" />
 						</ul>
 					</section>
-					<section>
+					<section className="flex flex__column flex__gap-2">
 						<h2 className="heading-3">Tillagas</h2>
 						<ul className="order-page__orders-container">
 							<Order orderId="di7bv" time="14.33" orderStatus="confirmed" />
@@ -26,7 +32,7 @@ export const OrderPage = () => {
 							<Order orderId="di7bv" time="14.33" orderStatus="confirmed" />
 						</ul>
 					</section>
-					<section>
+					<section className="flex flex__column flex__gap-2">
 						<h2 className="heading-3">Redo att hämtas</h2>
 						<ul className="order-page__orders-container">
 							<Order orderId="di7bv" time="14.33" orderStatus="done" />
