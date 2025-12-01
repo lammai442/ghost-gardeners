36d22e99a1b137713d5a90aa85642e849f779323
Stefan Mogren <stefan_mogren@hotmail.com>
2025-12-01 10:49:18 +0100

    Adjusted router

--- business/packages/core/router/data/index.tsx
+++ business/packages/core/router/data/index.tsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { HomePage } from '@mojjen/homepage';
 import { AuthPage } from '@mojjen/authpage';
+import App from '../../../../src/App';
 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/auth',
-    element: <AuthPage />,
-  },
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: '/auth',
+				element: <AuthPage />,
+			},
+		],
+	},
 ]);
