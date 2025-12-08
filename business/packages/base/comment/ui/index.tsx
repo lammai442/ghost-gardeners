26f3da7db546ed3c7813c73491e7e76e62750a6c
ninerino <ninesalternativemail@gmail.com>
2025-12-08 16:43:27 +0100

    Fixed business UI

--- business/packages/base/comment/ui/index.tsx
+++ business/packages/base/comment/ui/index.tsx
@@ -13,10 +13,8 @@ export const Comment = ({
 	handleChangeComment,
 }: Props) => {
 	return (
-		<div className="flex flex__column comment">
-			<h5 className="base-bold font-color-red text-ketchup">
-				Lägg till en kommentar
-			</h5>
+		<div className="flex flex__column comment flex__gap-0">
+			<p className="base-bold font-color-red text-ketchup">Lägg till en kommentar</p>
 			<p className="base-small">Viktig info till kollegorna</p>
 
 			<label className="flex comment__label">
@@ -26,7 +24,7 @@ export const Comment = ({
 					maxLength={200}
 					className="border-radius comment__textarea"
 					onChange={handleChangeComment}
-					placeholder="Glöm inte lägga till userComment också så personalen kan se det. Återstår att lösa backend så det faktiskt ändrar nåt mer än status. Verkar inte räkna tecken längre heller. Undrar vad jag tog bort. Klockan är 23 så jag ursäktar mig själv att det försvann"
+					placeholder="Till exempel: superallergisk, rengör stekbord innan tillagning påbörjas"
 				/>
 			</label>
 			<span className="base-small comment__max-characters">
@@ -41,5 +39,5 @@ export const Comment = ({
  * Created a textarea for comments.
  *
  * Edited by: ninerino
- * Stripped down to what we need in business
+ * Stripped down to what we need on the business side
  */
