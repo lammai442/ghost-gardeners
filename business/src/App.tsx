8b05c332066983afedd129d8a7bbc350bd732868
lammai442 <lam.mai442@gmail.com>
2025-12-12 14:23:14 +0100

    changes

--- business/src/App.tsx
+++ business/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.scss';
 import { Outlet } from 'react-router-dom';
 import { useWebSocketHook } from '@mojjen/usewebsockethook';
+import { useEffect } from 'react';
+import { useAuthStore } from '@mojjen/useauthstore';
+import { jwtDecode } from 'jwt-decode';
 
 /**
  * Modified: StefanMogren
@@ -9,6 +12,25 @@ import { useWebSocketHook } from '@mojjen/usewebsockethook';
  */
 
 function App() {
+	const { user, logout } = useAuthStore();
+
+	// If the token is expired the user gets logged out. The function checkTokenExp runs every 60 s.
+	useEffect(() => {
+		const checkTokenExp = () => {
+			if (!user?.token) return;
+			const decoded = jwtDecode(user.token);
+			if (!decoded.exp) return;
+
+			const isExpired = decoded.exp * 1000 < Date.now();
+			isExpired && logout();
+		};
+
+		checkTokenExp();
+
+		const interval = setInterval(checkTokenExp, 60000);
+		return () => clearInterval(interval);
+	}, [user, logout]);
+
 	useWebSocketHook();
 	return (
 		<>
