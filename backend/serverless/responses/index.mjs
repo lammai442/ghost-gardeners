efe22c7a7a79ab46e4f2fc8fc1931f45f5c7b80f
lammai442 <lam.mai442@gmail.com>
2025-12-12 11:20:09 +0100

    started on csp

--- backend/serverless/responses/index.mjs
+++ backend/serverless/responses/index.mjs
@@ -1,6 +1,17 @@
+import 'dotenv/config';
+
 export const sendResponses = (statusCode, data) => {
 	return {
 		statusCode: statusCode,
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Security-Policy': `default-src 'self'; 
+  			script-src 'self' https://cdnjs.cloudflare.com https://assets10.lottiefiles.com; 
+  			style-src 'self' https://fonts.googleapis.com; 
+  			font-src 'self' https://fonts.gstatic.com; 
+  			img-src 'self' data:; 
+			connect-src 'self' https://re7yle0qc7.execute-api.eu-north-1.amazonaws.com ws:;`,
+		},
 		body: JSON.stringify({
 			...data,
 		}),
@@ -10,4 +21,5 @@ export const sendResponses = (statusCode, data) => {
 /**
  * Author: Lam
  * Function to use when sending responses
+ * Added headers and CSP
  */
